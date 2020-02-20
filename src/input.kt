6eb66a9340bdc91a6a8698e3942215ceb8c5f463
Meghdut Mandal <meghdut.windows@gmail.com>
2020-02-21 00:51:30 +0530

    Added priority

--- src/input.kt
+++ src/input.kt
@@ -1,25 +1,43 @@
 import java.io.File
 
-data class Lib(val libid:Int,val bookPerDay:Int, val signUpTime:Int,val booksList: java.util.ArrayList<Book>,var maxScore:Int=0,var priority:Int=0)
-data class Book(val id:Int,val  score:Int)
+data class Lib(
+    val libid: Int,
+    val bookPerDay: Int,
+    val signUpTime: Int,
+    val booksList: java.util.ArrayList<Book>,
+    var maxScore: Int = 0,
+    var priority: Double = 0.0
+) {
+    fun calPriority() {
+        priority = ((signUpTime * 1.0) / (booksList.size)) * bookPerDay
+    }
+
+}
+
+
+
+data class Book(val id: Int, val score: Int)
 
 fun main() {
-val file = File("abc.txt")
+    val file = File("abc.txt")
     val readLines = file.readLines()
     val mainInfo = readLines[0].split(" ").map { it.toInt() }
-    val books=mainInfo[0]
-    val libraries=mainInfo[1]
-    val days=mainInfo[2]
+    val books = mainInfo[0]
+    val libraries = mainInfo[1]
+    val days = mainInfo[2]
 
     val scoresList = readLines[1].split(" ").map { it.toInt() }
-    val map = (0 until libraries)
-        .map { i ->
+    val libList= arrayListOf<Lib>()
+     (0 until libraries)
+        .forEach { i ->
             val libINfo = readLines[i + 2].split(" ").map { it.toInt() }
-            val bookInLib= arrayListOf<Book>()
-           readLines[i + 3].split(" ").map { it.toInt() }.map { Book(it, scoresList[it]) }.toMutableList().forEach {
-               bookInLib.add(it)
-           }
-            Lib(i,libINfo[2], libINfo[1], bookInLib,0)
+            val bookInLib = arrayListOf<Book>()
+            readLines[i + 3].split(" ").map { it.toInt() }.map { Book(it, scoresList[it]) }.toMutableList().forEach {
+                bookInLib.add(it)
+            }
+            libList.add(Lib(i, libINfo[2], libINfo[1], bookInLib, 0))
         }
+
+    Main.solve(books, days,libList)
 
 }
